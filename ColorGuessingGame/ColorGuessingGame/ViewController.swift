a58207744362293866919ce19ce10f254182b82d
Anna Friemoth <annafriemoth@gmail.com>
2019-10-30 15:48:59 -0400

    fixed view

--- ColorGuessingGame/ColorGuessingGame/ViewController.swift
+++ ColorGuessingGame/ColorGuessingGame/ViewController.swift
@@ -12,38 +12,34 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var randomColorBox: UIView!
-    let myColor = UIColor(red: CGFloat.random(in: 0...1), green: CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: CGFloat.random(in: 0...1))
-
     
+    var myColor = UIColor(red: CGFloat.random(in: 0...1),
+                          green: CGFloat.random(in: 0...1),
+                          blue: CGFloat.random(in: 0...1),
+                          alpha: CGFloat.random(in: 0...1))
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // var score =
         
-        //test
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+        randomColorBox.backgroundColor = myColor
+        
     }
 
+    
 
     // let randNum = CGFloat.random(in: 0...1)
    
     
-    @IBAction func red(_ sender: UIButton) {
+    @IBAction func buttons(_ sender: UIButton) {
         if sender.isTouchInside {
-            randomColorBox.backgroundColor = myColor
+            //randomColorBox.backgroundColor = myColor
         }
     }
     
-    @IBAction func green(_ sender: UIButton) {
-        if sender.isTouchInside {
-            randomColorBox.backgroundColor = myColor
-        }
-    }
-    
-    @IBAction func blue(_ sender: UIButton) {
-        if sender.isTouchInside {
-                   randomColorBox.backgroundColor = myColor
-        }
-    }
+  
+
     
 }
 
